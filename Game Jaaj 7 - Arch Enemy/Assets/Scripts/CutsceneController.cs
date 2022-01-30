e611ed79f8f5e861d9a57d4f37e38ad932d09d18
Alisson-Henrique <torgatesousa@gmail.com>
2022-01-30 13:01:38 -0300

    Cutcenes upgrades

--- Game Jaaj 7 - Arch Enemy/Assets/Scripts/CutsceneController.cs
+++ Game Jaaj 7 - Arch Enemy/Assets/Scripts/CutsceneController.cs
@@ -1,14 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CutsceneController : MonoBehaviour
 {
     [SerializeField]
     private GameObject FadeOut;
 
+    private AudioSource audioSource;
+
+    private GameController gameController;
+
+    public Image ilustrationSprite;
+
     public void PlayFadeOut()
     {
         FadeOut.SetActive(true);
+    }
+
+    void Start()
+    {
+        gameController = FindObjectOfType(typeof(GameController)) as GameController;
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = gameController.GetCutsceneAudioClip();
+        ilustrationSprite.sprite = gameController.GetIlustratio();
+        audioSource.Play();
     }
 }
