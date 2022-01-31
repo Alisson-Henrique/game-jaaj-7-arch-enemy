0ff2edadc822955cd1737ef3d98db79b266e5a44
Vinícius Viana <vinicius.viana1992@gmail.com>
2022-01-30 21:37:57 -0300

    Add card selected

--- Game Jaaj 7 - Arch Enemy/Assets/Scripts/BattleHUD.cs
+++ Game Jaaj 7 - Arch Enemy/Assets/Scripts/BattleHUD.cs
@@ -14,10 +14,18 @@ public class BattleHUD : MonoBehaviour
 	public GameObject selectedCard;
 	public Image selectedBorder;
 
-	public GameObject cardVFX;
+	public GameObject cardSelectedVFX;
+	public AudioClip cardSelectedSFX;
 
 	public GameObject hunter;
 	public GameObject monster;
+
+    private AudioSource audioSource;
+
+	void Awake() 
+	{
+		audioSource = GetComponent<AudioSource>();
+	}
 
 	public void SetHUD(Unit unit)
 	{
@@ -41,18 +49,21 @@ public class BattleHUD : MonoBehaviour
 		selectedCard.SetActive(true);
 		selectedCard.GetComponent<Image>().sprite = card.image;
 		selectedBorder.sprite = card.border;
-
-		var cardSelectionVFX = Instantiate(cardVFX);
+       
+		audioSource.clip = cardSelectedSFX;
+	    audioSource.Play();
+		
+		var cardSelectedVFXInstance = Instantiate(cardSelectedVFX);
 		yield return new WaitForSeconds(2f);
-		Destroy(cardSelectionVFX);	
+		Destroy(cardSelectedVFXInstance);	
 		selectedCard.SetActive(false);
 
 		// if (card.cardVFX != null)
 		// {
 		// 	var cardAnimationVFXTransform = card.target == 0 ? hunter.transform : monster.transform;
-		// 	var cardAnimationVFX = Instantiate(card.cardVFX);
+		// 	var cardAnimationVFXInstance = Instantiate(card.cardVFX, cardAnimationVFXTransform);
 		// 	yield return new WaitForSeconds(4f);
-		// 	Destroy(cardAnimationVFX); 
+		// 	Destroy(cardAnimationVFXInstance); 
 		// }
 	}
 
